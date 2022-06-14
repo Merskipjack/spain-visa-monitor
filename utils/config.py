3e98588e4f6f0bba0ae8f73f8566e4c80eb74d65
Merskipjack <107508572+Merskipjack@users.noreply.github.com>
2022-06-14 21:01:00 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -6,6 +6,6 @@ TIMEOUT = 90  # refresh page every 90s
 CENTER = ('Scotland', 'Edinburgh', 'Normal', 'Tourism')
 # CENTER = ('Bristol', 'London', 'Normal', 'Tourist')
 # =============== PERSONAL CONFIG FOR VISA ===============
-OPENED_PAGE = 'https://uk.blsspainvisa.com/visa4spain/book-date/QwertYU123'  # your BLS appointment page link
+OPENED_PAGE = 'https://uk.blsspainvisa.com/visa4spain/book-date/X6WopZGcig'  # your BLS appointment page link
 EMAIL = '18600906630@163.com'  # BLS email
 PASSWORD = 'Song-888'  # BLS password
